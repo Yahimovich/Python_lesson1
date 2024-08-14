0bc37f07e219bc072508bee62b56762af974704f
unknown <dobraya_vedma@bk.ru>
2024-08-14 15:39:34 +0700

    lesson5

---
+++ lesson_5/scripts/task3_1.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+driver = webdriver.Chrome()
+driver.get("http://the-internet.herokuapp.com/add_remove_elements/")
+for _ in range(5):
+    add_button = driver.find_element(
+        By.XPATH, "//button[text()='Add Element']").click()
+    delete_button = driver.find_elements(
+        "xpath", "//button[text()='Delete']"
+    )
+sleep (4)
+print(f"Количество кнопок 'Delete': {len(delete_button)}")
+driver.quit()
+
+driver1 = webdriver.Firefox()
+driver1.get("http://the-internet.herokuapp.com/add_remove_elements/")
+for _ in range(5):
+    add_button = driver1.find_element(
+        By.XPATH, "//button[text()='Add Element']").click()
+    delete_button = driver1.find_elements(
+        "xpath", "//button[text()='Delete']"
+    )
+sleep (4)
+print(f"Количество кнопок 'Delete': {len(delete_button)}")
+driver1.quit()
---
+++ lesson_5/task3_4.py
@@ -0,0 +1,29 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("http://the-internet.herokuapp.com/entry_ad")
+wait = WebDriverWait(driver, 10)
+modal_window = wait.until(
+    EC.visibility_of_element_located((By.CSS_SELECTOR,".modal"))
+)
+close_button = wait.until(EC.element_to_be_clickable(
+    (By.CSS_SELECTOR, "modal-footer")
+))
+sleep(3)
+driver.quit()
+
+driver = webdriver.Firefox()
+driver.get("http://the-internet.herokuapp.com/entry_ad")
+wait = WebDriverWait(driver, 10)
+modal_window = wait.until(
+    EC.visibility_of_element_located((By.CSS_SELECTOR,".modal"))
+)
+close_button = wait.until(EC.element_to_be_clickable(
+    (By.CSS_SELECTOR, "modal-footer")
+))
+sleep(3)
+driver.quit()
